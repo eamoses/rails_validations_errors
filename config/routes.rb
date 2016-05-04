888914411456d750961da4ce3c894bf72d0121b1
eamoses <e.a.moses@gmail.com>
2016-05-04 13:29:21 -0700

    commented out routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,3 +7,22 @@ Rails.application.routes.draw do
   end
 
 end
+
+# Prefix Verb   URI Pattern                               Controller#Action
+#           root GET    /                                         site#index
+#     owner_pets GET    /owners/:owner_id/pets(.:format)          pets#index
+#                POST   /owners/:owner_id/pets(.:format)          pets#create
+#  new_owner_pet GET    /owners/:owner_id/pets/new(.:format)      pets#new
+# edit_owner_pet GET    /owners/:owner_id/pets/:id/edit(.:format) pets#edit
+#      owner_pet GET    /owners/:owner_id/pets/:id(.:format)      pets#show
+#                PATCH  /owners/:owner_id/pets/:id(.:format)      pets#update
+#                PUT    /owners/:owner_id/pets/:id(.:format)      pets#update
+#                DELETE /owners/:owner_id/pets/:id(.:format)      pets#destroy
+#         owners GET    /owners(.:format)                         owners#index
+#                POST   /owners(.:format)                         owners#create
+#      new_owner GET    /owners/new(.:format)                     owners#new
+#     edit_owner GET    /owners/:id/edit(.:format)                owners#edit
+#          owner GET    /owners/:id(.:format)                     owners#show
+#                PATCH  /owners/:id(.:format)                     owners#update
+#                PUT    /owners/:id(.:format)                     owners#update
+#                DELETE /owners/:id(.:format)                     owners#destroy
