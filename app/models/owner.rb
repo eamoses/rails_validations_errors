97c788a824adc1d6e413f51f321cc3a50fad1037
eamoses <e.a.moses@gmail.com>
2016-05-04 13:14:03 -0700

    added validations

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,6 +1,7 @@
 class Owner < ActiveRecord::Base
-  # TODO: add validations
-
+  validates :first_name, presence: true, length: {maximum: 255}
+  validates :last_name, presence: true, length: {maximum: 255}
+  validates :email, presence: true, uniqueness: true, length: {maximum: 255}
   before_save :normalize_phone_number
 
   # removes leading 1 and the characters (, ), -, .
